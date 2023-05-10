370adf770fc61c5c533803924348aac71c8f9a3e
YQ Tian <yueqian.tian@hotmail.com>
2023-05-10 10:31:14 +0800

    update config

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import App from './App.vue';
 import router from './router/index.js';
 import store from './store/index.js';
 
-import './assets/tailwind.css';
+import '../tailwind.css';
 
 const app = createApp(App);
 
