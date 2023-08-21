6540a5308dbb35de92110a31a869fe2aaae80015
YQ Tian <yueqian.tian@hotmail.com>
2023-08-21 17:46:10 +0800

    config-updated-1.1

--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -4,7 +4,7 @@ export const AMAP_API_KEY = 'dfcf954e586ba34012fe3702733a0429'; // github demo
 // APISPACE API: https://www.apispace.com/about
 export const APISPACE_API_KEY = '78aca36112124475ae836a484406ea20'; // github demo
 
-export const REQUEST_BASE_URL_APISPACE = 'https://www.apispace.com/';
+export const REQUEST_BASE_URL_APISPACE = 'https://www.apispace.com';
 
 export const FCST_HOURS = 48;
 export const FCST_DAYS = 10;
